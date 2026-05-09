13ab75a7e25a054e209ef87a276e2ea6e1cea025
lscheiner <leandro.capinan@gmail.com>
2026-05-08 22:52:42 -0300

    init project

---
+++ src/main/java/br/com/scheiner/sqs_consumer/SqsConsumerApplication.java
@@ -0,0 +1,15 @@
+package br.com.scheiner.sqs_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class SqsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsConsumerApplication.class, args);
+	}
+
+}
